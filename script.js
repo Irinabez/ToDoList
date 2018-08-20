c1819985d01e40d993c9e48b87be8b2977271a21
Irina Bezrodnaya <irinabez@gmail.com>
2018-08-20 16:33:12 -0700

    added eslint config

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const list = [
+  { title: 'First', done: false},
+];
+renderList();
+function addToDo() {
+  const todoInputValue = document.getElementById('input').value;
+  list.push({title: todoInputValue, done: false});
 
+  console.log(list);
+  renderList();
+}
+
+function renderList() {
+  const ul = document.getElementById('list');
+  let li;
+  let textForLi;
+  ul.innerHTML = '';
+
+  list.forEach(item => {
+    li = document.getElementById('li');
+    textForLi = document.createTextNode(item.title);
+    if(item.done) li.className = 'done';
+    li.appendChild(textForLi);
+    ul.appendChild(li);
+  });
+}
